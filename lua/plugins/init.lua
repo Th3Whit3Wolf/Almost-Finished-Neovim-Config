47c654a9164ac99f89b851a82aeb986e471bdfcb
David Karrick <the.white.wolf.is.1337@gmail.com>
2020-10-09 14:04:54 +0100

    Simplified lua packadd plugins

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,26 +1,25 @@
 local plugins = {
-"nvim-lspconfig", -- Collection of common configurations for the Nvim LSP client.
-"lsp_extensions.nvim", -- bunch of info & extension callbacks for built-in LSP (provides inlay hints)
-"completion-nvim", -- auto completion framework that aims to provide a better completion experience with neovim's built-in LSP
-"diagnostic-nvim", -- wrapsthe diagnostics setting to make it more user friendly
-"nvim-treesitter", -- Treesitter configurations and abstraction layer for Neovim. 
-"completion-treesitter", -- treesitter based completion sources.
-"completion-buffers", -- completion for buffers word.
-"vim-dadbod-completion", -- completion sources for vim-dadbod
-"completion-tags", -- Slightly improved ctags completion
-"nvim-web-devicons", -- devicons in lua
-"nvim-tree.lua", -- A File Explorer For Neovim Written In Lua
-"snippets.nvim", -- Snippets tool written in lua
-"nvim-colorizer.lua", -- high-performance color highlighter for Neovim
-"formatter.nvim", -- A format runner for neovim, written in lua
-"space-nvim-theme" -- My spacemacs inspired theme
+    "nvim-lspconfig", -- Collection of common configurations for the Nvim LSP client.
+    "lsp_extensions.nvim", -- bunch of info & extension callbacks for built-in LSP (provides inlay hints)
+    "completion-nvim", -- auto completion framework that aims to provide a better completion experience with neovim's built-in LSP
+    "diagnostic-nvim", -- wrapsthe diagnostics setting to make it more user friendly
+    -- "nvim-treesitter", -- Treesitter configurations and abstraction layer for Neovim. 
+    -- "completion-treesitter", -- treesitter based completion sources.
+    "completion-buffers", -- completion for buffers word.
+    "vim-dadbod-completion", -- completion sources for vim-dadbod
+    -- "completion-tags", -- Slightly improved ctags completion
+    "nvim-web-devicons", -- devicons in lua
+    "nvim-tree.lua", -- A File Explorer For Neovim Written In Lua
+    "snippets.nvim", -- Snippets tool written in lua
+    "nvim-colorizer.lua", -- high-performance color highlighter for Neovim
+    "formatter.nvim", -- A format runner for neovim, written in lua
+    "space-nvim-theme", -- My spacemacs inspired theme
+    "plenary.nvim"
 }
 
 for _, plugin in ipairs(plugins) do
-    -- Actual lazy loaded lua plugins
-    if plugin ~= 'nvim-treesitter' or plugin ~= 'completion-treesitter' or plugin ~= 'completion-tags' then
-        vim.cmd('packadd ' .. plugin)
-    end
+    -- Temporary work around for using lua plugins
+    vim.cmd('packadd ' .. plugin)
 end
 
 require 'plugins/lsp'
