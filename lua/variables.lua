c061f5f420f36987d000a2ba9599c0343dde6d43
David Karrick <the.white.wolf.is.1337@gmail.com>
2020-10-09 14:05:07 +0100

    Set variables for nvim completion

--- lua/variables.lua
+++ lua/variables.lua
@@ -24,15 +24,24 @@ function variables:load_variables()
     if global.exists(global.node) then
         self.node_host_prog = global.node
     end
-    -- Diagnostic-nvim settings
+    -- Diagnostic Nvim settings
     self.diagnostic_insert_delay = 1
     self.diagnostic_show_sign = 1
     self.diagnostic_enable_underline = 0
     self.diagnostic_auto_popup_while_jump = 1
     self.diagnostic_enable_virtual_text = 1
 
+    -- Completion Nvim settings
     -- Fix for completion with other plugins mapped to enter
     self.completion_confirm_key = ""
+    -- Enable auto signature
+    self.completion_enable_auto_signature = 1
+    -- Auto change completion source
+    self.completion_auto_change_source = 1
+    -- Complete on delete
+    self.completion_trigger_on_delete = 1
+    -- ignore case matching
+    self.completion_matching_ignore_case = 1
 
     -- Disable provider for perl, python2, & ruby
     self.loaded_python_provider = 0
