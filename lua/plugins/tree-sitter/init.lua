527404daeecc8d40ccb8180eb03d6d6201b42479
David Karrick <the.white.wolf.is.1337@gmail.com>
2020-10-14 08:25:43 +0100

    Make nvim do more things

--- lua/plugins/tree-sitter/init.lua
+++ lua/plugins/tree-sitter/init.lua
@@ -1,29 +1,68 @@
 require'nvim-treesitter.configs'.setup {
+    highlight = {
+        enable = true,
+        disable = {},
+        custom_captures = {}
+    },
+    incremental_selection = {
+        enable = true,
+        disable = {},
+        keymaps = { -- mappings for incremental selection (visual mappings)
+            init_selection = 'gnn', -- maps in normal mode to init the node/scope selection
+            node_incremental = "grn", -- increment to the upper named parent
+            scope_incremental = "grc", -- increment to the upper scope (as defined in locals.scm)
+            node_decremental = "grm" -- decrement to the previous node
+        }
+    },
     refactor = {
-        ensure_installed = "all",
-        highlight = {
+        highlight_definitions = {
             enable = true
         },
         highlight_current_scope = {
-            -- enable = true
-        },
-        highlight_definitions = {
-            -- enable = true
+            enable = true
         },
         smart_rename = {
             enable = true,
             keymaps = {
-                smart_rename = "crn"
+                smart_rename = "grr" -- mapping to rename reference under cursor
             }
         },
         navigation = {
             enable = true,
             keymaps = {
-                goto_definition_lsp_fallback = "gd",
-                list_definitions = "gD",
-                goto_next_usage = "gnd",
-                goto_previous_usage = "gpd"
+                goto_definition = "gnd", -- mapping to go to definition of symbol under cursor
+                list_definitions = "gnD" -- mapping to list all definitions in current file
             }
         }
-    }
+    },
+    textobjects = { -- syntax-aware textobjects
+        enable = true,
+        disable = {},
+        keymaps = {
+            -- ["iL"] = { -- you can define your own textobjects directly here
+            --   python = "(function_definition) @function",
+            --   cpp = "(function_definition) @function",
+            --   c = "(function_definition) @function",
+            --   java = "(method_declaration) @function"
+            -- },
+            -- or you use the queries from supported languages with textobjects.scm
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+            ["aC"] = "@class.outer",
+            ["iC"] = "@class.inner",
+            ["ac"] = "@conditional.outer",
+            ["ic"] = "@conditional.inner",
+            ["ae"] = "@block.outer",
+            ["ie"] = "@block.inner",
+            ["al"] = "@loop.outer",
+            ["il"] = "@loop.inner",
+            ["is"] = "@statement.inner",
+            ["as"] = "@statement.outer",
+            ["ad"] = "@comment.outer",
+            ["am"] = "@call.outer",
+            ["im"] = "@call.inner"
+        }
+    },
+    ensure_installed = 'all' -- one of 'all', 'language', or a list of languages
 }
+
