1307d184b4305ac6998cf5010dd0ab07ad309b6e
David Karrick <the.white.wolf.is.1337@gmail.com>
2020-11-01 16:10:10 +0000

    Setup Dusk til Dawn

---
+++ lua/plugins/dusk_til_dawn/init.lua
@@ -0,0 +1,7 @@
+vim.g.dusk_til_dawn_light_luafile = '~/.local/share/nvim/site/pack/packer/opt/space-nvim-theme/lua/space-nvim-theme.lua'
+vim.g.dusk_til_dawn_dark_luafile  = '~/.local/share/nvim/site/pack/packer/opt/space-nvim-theme/lua/space-nvim-theme.lua'
+
+vim.g.dusk_til_dawn_morning = 7
+vim.g.dusk_til_dawn_night = 19
+
+require 'Dusk-til-Dawn'
