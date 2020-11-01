01f00f08f02707fd4d4610ad4dee549923189566
David Karrick <the.white.wolf.is.1337@gmail.com>
2020-11-01 16:11:15 +0000

    Added plugin counter

--- lua/utils/paths.lua
+++ lua/utils/paths.lua
@@ -21,20 +21,40 @@ function reversedipairs(t)
     return reversedipairsiter, t, #t + 1
 end
 
+
+
 function path.to_table()
     local paths = Split(package.path, ";")
-    ---print(vim.inspect(paths))
+    print(vim.inspect(paths))
     for k,_ in pairs(paths) do
-        local dirs = Split(paths[k], "/")
-        for i=1, #dirs do
-            if dirs[#dirs + 1 - i] == 'nvim-lspconfig' then
-                print(paths[k])
-                local str = table.concat(dirs , '/' , 1 , #dirs + 1 - i )
-                print("path: " .. str)
-                print(dirs[#dirs + 1 - i])
-            end
+        if string.match(paths[k], "nvim/site/pack/") then
+        count = count + 1
         end
     end
+    print('plugins: ' .. count)
+end
+
+
+function path.lua_plugins()
+    local len = string.len(vim.fn.stdpath("config") .. '/lua/') + 1
+    for _,k in pairs(vim.fn.systemlist("fd . $HOME'/.config/nvim/lua/plugins' -t d -d 1")) do
+        print(k)
+        print(string.sub(k, len))
+    end
+end
+
+function path.totalPlugins()
+    local count = 0
+    paths = {};
+    for match in (package.path..';'):gmatch("(.-)"..';') do
+        table.insert(paths, match);
+    end
+    for k,_ in pairs(paths) do
+        if string.match(paths[k], "nvim/site/pack/") then
+        count = count + 1
+        end
+    end
+    return count
 end
 
 return path
