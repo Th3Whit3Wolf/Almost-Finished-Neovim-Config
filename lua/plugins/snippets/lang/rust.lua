3ae55aba9aabd063e480aa6167f376bb56a2377e
David Karrick <the.white.wolf.is.1337@gmail.com>
2020-11-01 16:08:46 +0000

    Setup language specific snippets

--- lua/plugins/snippets/lang/rust.lua
+++ lua/plugins/snippets/lang/rust.lua
@@ -1,51 +1,43 @@
+local U = require 'snippets.utils'
+
 local snippets = {}
 
-function snippets.gen_snippets()
-    rust = {
-        ["afn"] = U.match_indentation [[
-async fn $1($2) -> $3 {
-    $0
-}
-]],
-
-        macro = U.match_indentation [[
-macro_rules! ${1:name} {
+snippets = {
+    macro = U.match_indentation [[
+macro_rules! ${1:name} { 
   ($2) => {
     $0
   }
 }
 ]],
-        type = [[type $1 = $2;]],
-        struct = U.match_indentation [[
+    type = [[type $1 = $2;]],
+    struct = U.match_indentation [[
 struct $1 {
   $0
 }]],
-        enum = U.match_indentation [[
+    enum = U.match_indentation [[
 enum $1 {
   $0
 }]],
-        -- TODO(ashkan, 2020-08-19 05:33:54+0900) case change from TitleCase to snake_case for last element of ::
-        field = [[$1: $2,]],
-        -- field = [[${2=R.case_change.S[1]..}: $1,]];
-        impl = U.match_indentation [[
+    field = [[$1: $2,]],
+    -- field = [[${2=R.case_change.S[1]..}: $1,]];
+    impl = U.match_indentation [[
 impl $1 {
-$0
+  $0
 }
 ]],
-        hashmap = [[use std::collections::HashMap;]],
-        hashset = [[use std::collections::HashSet;]],
-        collections = [[use std::collections::$1;]],
-        match = U.match_indentation [[
+    hashmap = [[use std::collections::HashMap;]],
+    hashset = [[use std::collections::HashSet;]],
+    collections = [[use std::collections::$1;]],
+    match = U.match_indentation [[
 match $1 {
   $0
 }]],
-        bcase = U.match_indentation [[
+    bcase = U.match_indentation [[
 $1 => {
   $0
 }]],
-        case = U.match_indentation [[$1 => $0,]]
-    };
-    return rust
-end
+    case = U.match_indentation [[$1 => $0,]]
+}
 
 return snippets
